7df485e1166eabc2eaaebdde8e6392aa2075e053
roldel <roldel@protonmail.com>
2021-06-29 22:39:22 +0000

    Initial commit
    Initial commit containing whole project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p> Welcome </p>
+  <teleport to=".modals" v-if="showModal"> 
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="">Sign Up</a>
+        <a href="">More info</a>
+      </template>
+        <h1>Ninja giveaway</h1>
+        <p>Grab your ninja discount now !!!</p>
+
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModalTwo"> 
+    <Modal @close="toggleModalTwo">
+        <h1>Sign up to newsletter</h1>
+        <p>For updates and promo codes</p>
+    </Modal>
+  </teleport>
+
+
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal</button>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components  : { Modal },
+  data (){
+    return {
+    title : "My first vue app :)",
+    showModal : false,
+    showModalTwo: false,
+    }
+  },
+  methods : {
+    handleClick (){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modlas {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1 px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
